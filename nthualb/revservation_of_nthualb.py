7b0dcb4147e6fd22e5bff2a8026d0d23666f8bca
iwtba4188 <X69599596@gmail.com>
2022-03-17 00:41:09 +0800

    220317 - modifying some detail
    Modify
    - adding option for Edge driver.
    - decreasing the waiting time and running efficiency.
    - refreshing the website will happen after going to next day, to avoid high refreshing speed of the web.
    - now, the log function can modifying as no delay mode.

--- nthualb/revservation_of_nthualb.py
+++ nthualb/revservation_of_nthualb.py
@@ -3,13 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 import datetime
 import time
 import os
+import sys
 
 
-WAITING_TIME = 2
+DRIVER_TYPE = "Edge"  # Chrome or Edge
+WAITING_TIME = 0.5
 
 
 with open("setting.txt", "r") as file:
@@ -27,8 +30,9 @@ with open("setting.txt", "r") as file:
     TARGET_DATE = datas[4]
 
 
-def log(log_message):
-    time.sleep(0.5)
+def log(log_message, time_limit=True):
+    if time_limit:
+        time.sleep(0.1)
     now_time = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     print(f"[Log: {now_time}] {log_message}")
 
@@ -37,7 +41,14 @@ def log(log_message):
 
 def crawling():
     log("open the webdriver")
-    nthualb = webdriver.Chrome(ChromeDriverManager().install())
+    if (DRIVER_TYPE == "Edge"):
+        nthualb = webdriver.Edge(EdgeChromiumDriverManager().install())
+    elif (DRIVER_TYPE == "Chrome"):
+        nthualb = webdriver.Chrome(ChromeDriverManager().install())
+    else:
+        sys.stderr.write("Unknow driver type")
+        sys.exit(1)
+
     log("set implicitly waitting time as 10sec")
     nthualb.implicitly_wait(10)
     log("enter the website to login")
@@ -54,7 +65,7 @@ def crawling():
     captcha_blank.click()
 
     # modify to "press any key to continue"
-    print("Already login?")
+    log("Already login?")
     os.system("pause")
 
     log("enter the website to last day")
@@ -62,25 +73,29 @@ def crawling():
 
     log("get last_date")
     last_date = nthualb.find_element_by_xpath("/html/body/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table[1]/tbody/tr/td[5]/div[2]").text
-    log("waiting for target_date")
+    log("waiting for 00:00")
+    while (datetime.datetime.now().strftime("%Y-%m-%d") != TARGET_DATE):
+        log("waiting for target_date")
     while (last_date != TARGET_DATE):
-        log("target_date not found")
-        log(f"sleep for {WAITING_TIME} secs then refresh")
+        log("target_date not found", False)
+        log(f"sleep for {WAITING_TIME} secs then refresh", False)
         time.sleep(WAITING_TIME)
-        log("refresh the website")
+        log("refresh the website", False)
         nthualb.refresh()
-        log("refresh last_date")
+        log("refresh last_date", False)
         last_date = nthualb.find_element_by_xpath(
             "/html/body/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table[1]/tbody/tr/td[5]/div[2]").text
 
-    log("target_date found!")
+    log("target_date found!", False)
 
-    log("select rows")
+    log("select rows", False)
+    WebDriverWait(nthualb, 10).until(EC.presence_of_element_located(
+        (By.XPATH, "/html/body/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table[2]/tbody/tr")))
     select_table = nthualb.find_elements_by_xpath("/html/body/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table[2]/tbody/tr")
     select_table.pop()
-    log("select col and select the button")
+    log("select col and select the button", False)
     place_button1 = select_table[TIME1].find_elements_by_tag_name("td")[HORI1].find_element_by_tag_name("div")
-    log("click the button")
+    log("click the button", False)
     place_button1.click()
 
     log("wait for alert appear")
@@ -94,8 +109,6 @@ def crawling():
 
     log("reget the website")
     nthualb.get("https://nthualb.url.tw/reservation/reservation?d=4")
-    log(f"sleep for {WAITING_TIME} secs")
-    time.sleep(WAITING_TIME)
     log("select rows")
     select_table = nthualb.find_elements_by_xpath("/html/body/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table[2]/tbody/tr")
     select_table.pop()
